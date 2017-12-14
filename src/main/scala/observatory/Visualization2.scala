52e6516293a741be89d58296351800fdfd68025e
ilnur serbaev <serbaevilnur@gmail.com>
2017-12-14 11:21:05 +0500

    week 5 - 9.0

--- src/main/scala/observatory/Visualization2.scala
+++ src/main/scala/observatory/Visualization2.scala
@@ -1,6 +1,8 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import scala.math._
+import Visualization._
 
 /**
   * 5th milestone: value-added information visualization
@@ -23,7 +25,9 @@ object Visualization2 {
     d10: Temperature,
     d11: Temperature
   ): Temperature = {
-    ???
+    val x = point.x
+    val y = point.y
+    d00 * (1 - x) * (1 - y) + d10 * x  * (1 - y) + d01 * (1 - x) * y + d11 * x * y
   }
 
   /**
@@ -37,7 +41,50 @@ object Visualization2 {
     colors: Iterable[(Temperature, Color)],
     tile: Tile
   ): Image = {
-    ???
+    val imageWidth = 256
+    val imageHeight = 256
+
+    val pixels = pixelLocations(tile, imageWidth, imageHeight).par.map{
+      case (pos,pixelLocation) => {
+
+        val latRange = List(floor(pixelLocation.lat).toInt, ceil(pixelLocation.lat).toInt)
+        val lonRange = List(floor(pixelLocation.lon).toInt, ceil(pixelLocation.lon).toInt)
+
+        val d = {
+          for {
+            xPos <- 0 to 1
+            yPos <- 0 to 1
+          } yield (xPos, yPos) -> grid(GridLocation(latRange(1 - yPos), lonRange(xPos)))
+        }.toMap
+
+        val xFraction = pixelLocation.lon - lonRange.head
+        val yFraction = latRange(1) - pixelLocation.lat
+
+
+        pos -> interpolateColor(
+          colors,
+          bilinearInterpolation(point = CellPoint(xFraction, yFraction), d00 = d((0,0)), d01 = d((0,1)), d10 = d((1,0)), d11 = d((1,1)))
+        ).pixel(127)
+      }}
+      .seq
+      .sortBy(_._1)
+      .map(_._2)
+
+
+    Image(imageWidth, imageHeight, pixels.toArray)
+  }
+
+  /**
+    * Generates a sequence of pos, location tuples for a Tile image
+    * @param imageWidth in pixels
+    * @param imageHeight in pixels
+    * @return 'Map' of (pos->Location) within the tile
+    */
+  def pixelLocations(tile: Tile, imageWidth: Int, imageHeight: Int): IndexedSeq[(Int, Location)] = {
+    for{
+      xPixel <- 0 until imageWidth
+      yPixel <- 0 until imageHeight
+    } yield xPixel + yPixel * imageWidth -> Tile(xPixel.toDouble / imageWidth + tile.y, yPixel.toDouble / imageHeight + tile.y, tile.zoom).location
   }
 
 }
